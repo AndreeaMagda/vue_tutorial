23d64ca094a12c9a268d28980093978fd4dc4ecb
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2023-04-12 14:06:30 +0300

    Attribute Bindings
     adding a dynamic class binding to the <h1>, using the titleClass data property as its value. If it's bound correctly, the text should turn violet.

---
+++ AttributeBindings.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      titleClass: 'title'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1 :class="titleClass">Make me violet</h1>
+</template>
+
+<style>
+.title {
+  color: violet;
+}
+</style>
