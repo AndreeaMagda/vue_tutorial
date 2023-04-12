ac2cb3422063c5ce8ee17828e9e7908e52c4f5c6
Magda Andreea <122922300+AndreeaMagda@users.noreply.github.com>
2023-04-12 14:19:47 +0300

    Conditional Rendering
    adding the v-if and v-else directives to them and implementing the toggle() method so that we can use the button to switch between them.

---
+++ ConditionalRendering.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      awesome: true
+    }
+  },
+  methods: {
+    toggle() {
+      this.awesome = !this.awesome
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="toggle">toggle</button>
+  <h1 v-if="awesome">Avada Kedavra🪄</h1>
+  <h1 v-else>Oh no, i am dead ☠️</h1>
+</template>
